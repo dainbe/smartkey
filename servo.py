e114431f6ca2f214c781b844cc6b763c40de4c58
dainbe <39964128+dainbe@users.noreply.github.com>
2018-07-03 23:02:36 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,35 +3,32 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)#BOARDにするとPIN番号指定
+GPIO.setmode(GPIO.BOARD)
 
 #PIN12を制御パルスの出力に設定
 gp_out = 12
 GPIO.setup(gp_out, GPIO.OUT)
 
-auto=1 #オートロック用
+global lock
+auto=0 #オートロック用
 lock=0 #0で開いてる１で閉まってる
 
 #入力の設定
 sw1_in=13
 sw2_in=15
-sw3_in=19
 reed=21
 GPIO.setup(sw1_in,GPIO.IN)
 GPIO.setup(sw2_in,GPIO.IN)
-GPIO.setup(sw3_in,GPIO.IN)
 GPIO.setup(reed,GPIO.IN)
 
 #出力の設定
-led1_out=15 #赤
-led2_out=16 #青
-led3_out=18 #緑
+led1_out=22#赤
+led2_out=24 #青
+led3_out=26 #緑
 GPIO.setup(led1_out,GPIO.OUT)
 GPIO.setup(led2_out,GPIO.OUT)
 GPIO.setup(led3_out,GPIO.OUT)
 
-#私の家のサムターンは+90°で開いて-90°で閉まる
-#180°サーボの場合デューティー比が2.5=-90°くらい 12=+90°くらい
 def open(): #鍵を開けるやつ
     GPIO.setup(gp_out,GPIO.OUT)
     time.sleep(0.5)
@@ -46,6 +43,7 @@ def open(): #鍵を開けるやつ
     time.sleep(0.5)
 
     GPIO.cleanup(gp_out)
+    global lock
 
     lock=0
 
@@ -64,6 +62,7 @@ def close(): #鍵を閉めるやつ
 
     GPIO.cleanup(gp_out)
 
+    global lock
     lock=1
 
 #GPIO.PWM( [ピン番号] , [周波数Hz] )
@@ -83,7 +82,7 @@ try:
 
             open()
 
-        elif GPIO.input(sw2_in)==1 and lock==0:
+        elif GPIO.input(sw1_in)==1 and lock==0:
             GPIO.output(led2_out,0)
             GPIO.output(led3_out,0)
 
@@ -91,20 +90,20 @@ try:
 
             close()
 
-        elif GPIO.input(sw3_in)==1:
+        elif GPIO.input(sw2_in)==1:
             #自動で閉まる
             GPIO.output(led1_out,0)
             GPIO.output(led2_out,0)
             GPIO.output(led3_out,1)
 
             while True:
-                if GPIO.input(reed)==1 and lock==0:
+                if GPIO.input(reed)==1:
                     if auto==0:
                         close()
                         auto=1
 
                     elif GPIO.input(sw1_in)==1 or GPIO.input(sw2_in)==1:
-                    break
+                        break
                 else :
                     auto=0
         else:
